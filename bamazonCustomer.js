c6de912b668237df83ab3e0b0c60141850c78fd4
Amy Wolf <awolgemuth@trinisys.loc>
2019-07-20 16:07:42 -0500

    Added prompts for product to buy and how many

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,30 +1,30 @@
-var mysql = require ("mysql");
-var inquirer = require ("inquirer");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "bamazon"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-    // runInventory();
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+  // runInventory();
 });
 
 // function runInventory () {
-    function afterConnection() {
-        connection.query("SELECT * FROM products", function(err, res) {
-          if (err) throw err;
-          // console.log(res);
-          inventoryList();
-          connection.end();
-        });
-      }
+function afterConnection() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    // console.log(res);
+    inventoryList();
+    connection.end();
+  });
+}
 
 //Running the app will first display all of the items available for sale.
 //Included are the ids, names, and prices of products for sale.
@@ -33,16 +33,31 @@ function inventoryList() {
   var query = "SELECT item_id, product_name, price FROM products";
   connection.query(query, function (err, res) {
     if (err) throw err;
+    console.log("-----Start: all products for sale------");
     for (var i = 0; i < res.length; i++) {
       console.log(
-        "Item ID: " + 
+        "Item ID: " +
         res[i].item_id +
         " || Product: " +
         res[i].product_name +
         " || Price: " +
         res[i].price
-        );
+      );
     }
+    console.log("-----End: all products for sale------");
+    inquirer
+      .prompt([
+        {
+          name: "whatToBuy",
+          type: "input",
+          message: "Enter the item ID of the product you would like to purchase: "
+        },
+        {
+          name: "howMany",
+          type: "input",
+          message: "How many of these do you wish to purchase? "
+        },
+      ])
   });
 }
 
